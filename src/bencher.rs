016d3966e416e6d743ad7807308e2b035ba0bbc6
ronakgh97 <ronakgh97@gmail.com>
2026-06-03 14:52:13 +0530

    feat: crypto bench

--- src/bencher.rs
+++ src/bencher.rs
@@ -1,23 +1,28 @@
-use crate::load::StaticMemPool;
+use crate::load::{NONCE_LEN, StaticMemPool, TAG_LEN, decrypt_buf, encrypt_buf};
 use anyhow::Context;
 use blas_rs::lvl3::gemm;
 use blas_rs::utils::Noise;
+use sha3::{Digest, Sha3_512};
 use std::hint::black_box;
 use std::sync::Arc;
 use std::time::Instant;
 
 const BASE_SCORE: usize = 1280;
-pub const SAMPLE_SIZE: usize = 2156;
+pub const MATRIX_SIZE: usize = 2048;
+pub const SAMPLE_SIZE: usize = MATRIX_SIZE * MATRIX_SIZE;
+pub const MAX_RUN: usize = 18;
+pub const MAX_WARMUP: usize = 8;
+pub const POOL_CAPACITY: usize = MAX_RUN * 3; // each bench run need 3 blocks
 
-pub async fn run_benchmark(runs: usize, warmups: usize, max_thread: usize) -> anyhow::Result<()> {
+pub async fn run_benchmark(warmups: usize, runs: usize, max_thread: usize) -> anyhow::Result<()> {
+    let mut noise = Noise::init();
     println!(
         "Warmup runs: {} Benchmark runs: {} Threads: {}\n",
         warmups, runs, max_thread
     );
-    println!("Running BLAS bench...");
 
-    let mem_pool = Arc::new(StaticMemPool::<[f32; SAMPLE_SIZE * SAMPLE_SIZE], 64>::init());
-    let mut noise = Noise::init();
+    println!("Running BLAS bench...");
+    let mem_pool = Arc::new(StaticMemPool::<[f32; SAMPLE_SIZE], { POOL_CAPACITY }>::init());
     let pool_clone = mem_pool.clone();
     tokio::spawn(async move {
         ctrl_c().await;
@@ -36,17 +41,17 @@ pub async fn run_benchmark(runs: usize, warmups: usize, max_thread: usize) -> an
         for _ in 0..warmups {
             let mut block_c = mem_pool.try_alloc_zeroed().context("Pool exhausted")?;
             gemm(
-                SAMPLE_SIZE,
-                SAMPLE_SIZE,
-                SAMPLE_SIZE,
+                MATRIX_SIZE,
+                MATRIX_SIZE,
+                MATRIX_SIZE,
                 1.0,
                 &*block_a,
-                SAMPLE_SIZE,
+                MATRIX_SIZE,
                 &*block_b,
-                SAMPLE_SIZE,
+                MATRIX_SIZE,
                 0.0,
                 &mut *block_c,
-                SAMPLE_SIZE,
+                MATRIX_SIZE,
                 false,
                 false,
             );
@@ -54,7 +59,7 @@ pub async fn run_benchmark(runs: usize, warmups: usize, max_thread: usize) -> an
         }
     }
 
-    let mut score = vec![(0.0f64, 0.0f64); runs];
+    let mut blas_score = vec![(0.0f64, 0.0f64); runs];
 
     #[allow(clippy::needless_range_loop)]
     for i in 0..runs {
@@ -67,57 +72,57 @@ pub async fn run_benchmark(runs: usize, warmups: usize, max_thread: usize) -> an
         if max_thread == 1 {
             let mut block_c = mem_pool.try_alloc_zeroed().context("Pool exhausted")?;
             gemm(
-                SAMPLE_SIZE,
-                SAMPLE_SIZE,
-                SAMPLE_SIZE,
+                MATRIX_SIZE,
+                MATRIX_SIZE,
+                MATRIX_SIZE,
                 1.0,
                 &*block_a,
-                SAMPLE_SIZE,
+                MATRIX_SIZE,
                 &*block_b,
-                SAMPLE_SIZE,
+                MATRIX_SIZE,
                 0.0,
                 &mut *block_c,
-                SAMPLE_SIZE,
+                MATRIX_SIZE,
                 false,
                 false,
             );
             black_box(&*block_c);
         } else {
             std::thread::scope(|s| {
-                let a_ref: &[f32] = block_a.get();
-                let b_ref: &[f32] = block_b.get();
+                let block_a: &[f32] = block_a.get();
+                let block_b: &[f32] = block_b.get();
                 for _ in 0..max_thread {
-                    let mut block = mem_pool
+                    let mut block_c = mem_pool
                         .try_alloc_zeroed()
                         .expect("Pool exhausted during thread allocation");
                     s.spawn(move || {
                         gemm(
-                            SAMPLE_SIZE,
-                            SAMPLE_SIZE,
-                            SAMPLE_SIZE,
+                            MATRIX_SIZE,
+                            MATRIX_SIZE,
+                            MATRIX_SIZE,
                             1.0,
-                            a_ref,
-                            SAMPLE_SIZE,
-                            b_ref,
-                            SAMPLE_SIZE,
+                            block_a,
+                            MATRIX_SIZE,
+                            block_b,
+                            MATRIX_SIZE,
                             0.0,
-                            block.get_mut(),
-                            SAMPLE_SIZE,
+                            block_c.get_mut(),
+                            MATRIX_SIZE,
                             false,
                             false,
                         );
-                        black_box(block.get());
+                        black_box(block_c.get());
                     });
                 }
             });
         }
         let elapsed = start.elapsed().as_secs_f64();
 
-        let flops_per_mul = 2.0 * (SAMPLE_SIZE as f64).powi(3) - (SAMPLE_SIZE as f64).powi(2);
+        let flops_per_mul = 2.0 * (MATRIX_SIZE as f64).powi(3) - (MATRIX_SIZE as f64).powi(2);
         let total_flops = flops_per_mul * (max_thread as f64);
         let gflops = total_flops / elapsed / 1e9;
 
-        score[i] = (elapsed, gflops);
+        blas_score[i] = (elapsed, gflops);
 
         println!(
             "Run {}: Time = {:.3?}s GFLOPS = {:.2}",
@@ -127,18 +132,123 @@ pub async fn run_benchmark(runs: usize, warmups: usize, max_thread: usize) -> an
         );
     }
 
-    // TODO: run crypto here
+    drop(mem_pool); // free the pool before crypto bench
 
-    println!("-------------------------------------");
-    let avg_gflops = score[..runs].iter().map(|s| s.1).sum::<f64>() / (runs as f64);
-    let total_time = score[..runs].iter().map(|s| s.0).sum::<f64>();
-    let cpu_score = BASE_SCORE + (avg_gflops + 1000.0) as usize;
+    let mut crypto_score = vec![(0.0f64, 0.0f64); runs];
+    println!("\nRunning Crypto bench...");
+    let mem_pool = Arc::new(StaticMemPool::<[u8; SAMPLE_SIZE], { POOL_CAPACITY * 3 }>::init());
+    let pool_clone = mem_pool.clone();
+    tokio::spawn(async move {
+        ctrl_c().await;
+        println!("Stopping...");
+        drop(pool_clone);
+        std::process::exit(0);
+    });
+
+    let mut key = [0u8; 32];
+    noise.fill_bytes(&mut key);
+
+    let plan_text_len = SAMPLE_SIZE - (NONCE_LEN + TAG_LEN);
+    #[allow(clippy::needless_range_loop)]
+    for i in 0..runs {
+        let mut block_a = mem_pool.try_alloc_zeroed().context("Pool exhausted")?;
+        let mut block_b = mem_pool.try_alloc_zeroed().context("Pool exhausted")?;
+        let mut block_c = mem_pool.try_alloc_zeroed().context("Pool exhausted")?;
+        noise.fill_bytes(&mut *block_a);
+        noise.fill_bytes(&mut *block_b);
+        noise.fill_bytes(&mut *block_c);
+        let start_time = Instant::now();
+        use core::arch::x86_64::{__rdtscp, _mm_lfence, _rdtsc};
+
+        let start = unsafe {
+            _mm_lfence();
+            _rdtsc()
+        };
+
+        if max_thread == 1 {
+            let plan_text: &mut [u8] = &mut block_a.get_mut()[..plan_text_len];
+            let cipher_text: &mut [u8] = block_b.get_mut();
+            black_box(&plan_text);
+            black_box(&cipher_text);
+            black_box(&block_c);
+
+            crypto_stress(plan_text, cipher_text, key, &mut *block_c)
+                .context("Crypto stress failed")?;
+        } else {
+            std::thread::scope(|s| {
+                for _ in 0..max_thread {
+                    let mut thread_block_a = mem_pool
+                        .try_alloc_zeroed()
+                        .expect("Pool exhausted during thread allocation");
+                    let mut thread_block_b = mem_pool
+                        .try_alloc_zeroed()
+                        .expect("Pool exhausted during thread allocation");
+                    let mut thread_block_c = mem_pool
+                        .try_alloc_zeroed()
+                        .expect("Pool exhausted during thread allocation");
+                    noise.fill_bytes(&mut *thread_block_a);
+                    noise.fill_bytes(&mut *thread_block_b);
+                    noise.fill_bytes(&mut *thread_block_c);
+
+                    s.spawn(move || {
+                        let plan_text: &mut [u8] = &mut thread_block_a.get_mut()[..plan_text_len];
+                        let cipher_text: &mut [u8] = thread_block_b.get_mut();
+                        black_box(&plan_text);
+                        black_box(&cipher_text);
+                        black_box(&thread_block_c);
+
+                        crypto_stress(plan_text, cipher_text, key, &mut *thread_block_c)
+                            .expect("Crypto stress failed");
+                    });
+                }
+            })
+        }
+
+        let mut aux = 0;
+        let end = unsafe {
+            let e = __rdtscp(&mut aux);
+            _mm_lfence();
+            e
+        };
+        let cycles = (end - start) as f64;
+        let elapsed = start_time.elapsed().as_secs_f64();
+        crypto_score[i] = (elapsed, cycles);
+
+        println!("Run {}: Time = {:.3?}s Cycles = {}", i + 1, elapsed, cycles);
+    }
+
+    println!("---------------------------------------");
+    let avg_gflops = blas_score[..runs].iter().map(|s| s.1).sum::<f64>() / (runs as f64);
+    let total_blas_time = blas_score[..runs].iter().map(|s| s.0).sum::<f64>();
+
+    let avg_crypto_cycles = crypto_score[..runs].iter().map(|s| s.1).sum::<f64>() / (runs as f64);
+    let total_crypto_time = crypto_score[..runs].iter().map(|s| s.0).sum::<f64>();
+
+    let cpu_score = BASE_SCORE + (avg_gflops + avg_crypto_cycles) as usize;
     println!("Estimated CPU Score: {}", cpu_score);
-    println!("Average GFLOPS/core: {:.2}", avg_gflops / max_thread as f64);
-    println!("Total time: {}s", total_time as f32);
+    println!("Average SCORE/Core: {:.2}", (cpu_score / max_thread) as f32);
+    println!("Total time: {:.2}s", total_blas_time + total_crypto_time);
 
     println!("Find your CPU here: https://boinc.bakerlab.org/rosetta/cpu_list.php");
 
+    Ok(())
+}
+
+fn crypto_stress(
+    plan_text: &mut [u8],
+    cipher_text: &mut [u8],
+    key: [u8; 32],
+    hash_buf: &mut [u8],
+) -> anyhow::Result<()> {
+    encrypt_buf(plan_text, cipher_text, &key)?;
+    decrypt_buf(cipher_text, plan_text, &key)?;
+    Sha3_512::digest(&hash_buf);
+    encrypt_buf(plan_text, cipher_text, &key)?;
+    decrypt_buf(cipher_text, plan_text, &key)?;
+    Sha3_512::digest(&hash_buf);
+    encrypt_buf(plan_text, cipher_text, &key)?;
+    decrypt_buf(cipher_text, plan_text, &key)?;
+    Sha3_512::digest(&hash_buf);
     Ok(())
 }
 
--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,12 +1,14 @@
+use cbench::bencher::{MAX_RUN, MAX_WARMUP, POOL_CAPACITY};
 #[allow(unused)]
 use cbench::bencher::{SAMPLE_SIZE, run_benchmark};
 use clap::{Parser, Subcommand};
+use std::cmp::min;
 
 #[derive(Parser)]
 #[command(
     name = "cbench",
     version = "1.0.0-alpha",
-    about = "BLAS based cpu micro-benching tool"
+    about = "BLAS, Crypto based cpu micro-benching tool"
 )]
 struct CliArgs {
     #[command(subcommand)]
@@ -29,6 +31,9 @@ enum Command {
         max_threads: usize,
     },
 }
+pub const MAX_THREAD: usize = 24;
+const OVERHEAD: usize = 20 * 1024 * 1024;
+const MAX_BYTES: usize = 4; // f32, u8 etc
 
 fn main() -> anyhow::Result<()> {
     let args = CliArgs::parse();
@@ -46,17 +51,21 @@ fn main() -> anyhow::Result<()> {
             let thread_num = if max_threads == 0 {
                 available_threads
             } else {
-                std::cmp::min(max_threads, available_threads)
+                min(max_threads, min(available_threads, MAX_THREAD))
             };
+
+            let runs = runs.clamp(1, MAX_RUN);
+            let warmups = warmups.clamp(1, MAX_WARMUP);
+            let stack_size = SAMPLE_SIZE * MAX_BYTES * POOL_CAPACITY + OVERHEAD;
 
             let main_handle = std::thread::Builder::new()
                 .name("bench-thread".to_string())
-                .stack_size(1_500_000_000)
+                .stack_size(stack_size) // oh, yeah
                 .spawn(move || {
                     let runtime =
                         tokio::runtime::Runtime::new().expect("Failed to create async runtime");
                     runtime.block_on(async {
-                        run_benchmark(runs, warmups, thread_num)
+                        run_benchmark(warmups, runs, thread_num)
                             .await
                             .expect("Benchmark failed");
                     });
